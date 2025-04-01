0492a68697a60771e43b58880c74fd5fa68b2e96
Paul Kang <kbu715@gmail.com>
2025-04-02 00:20:58 +0900

    baseUrl 대응

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,19 @@
+import { BASE_URL } from "./config";
 import { createObserver } from "./createObserver";
 
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => window.location.pathname.replace(BASE_URL, "/");
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, "", path);
+    window.history.pushState(
+      null,
+      "",
+      `${BASE_URL.replace(/\/+$/, "")}${path}`,
+    );
     notify();
   };
 

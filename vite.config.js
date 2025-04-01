655ddb7394027e654e2256db1f6ef865d48769f5
Paul Kang <kbu715@gmail.com>
2025-04-01 23:08:21 +0900

    vite config 설정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default mergeConfig(
-  defineConfig({
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const viteConfig = defineConfig({
+    base: env.VITE_BASE_URL,
     esbuild: {
       jsxFactory: "createVNode",
     },
@@ -12,13 +15,16 @@ export default mergeConfig(
         jsxFactory: "createVNode",
       },
     },
-  }),
-  defineTestConfig({
+  });
+
+  const testConfig = defineTestConfig({
     test: {
       globals: true,
       environment: "jsdom",
       setupFiles: "./src/setupTests.js",
       exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
     },
-  }),
-);
+  });
+
+  return mergeConfig(viteConfig, testConfig);
+};

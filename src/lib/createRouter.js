1f3f9555b44776e2dd153bc76be17280be249351
Paul Kang <kbu715@gmail.com>
2025-04-01 22:48:05 +0900

    createRouter - 경로에서 baseUrl 제거

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,17 @@
 import { createObserver } from "./createObserver";
 
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const removeBaseUrl = (path) => path.replace(baseUrl, "/");
+
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => removeBaseUrl(window.location.pathname);
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    window.history.pushState(null, null, removeBaseUrl(path));
     notify();
   };
 
